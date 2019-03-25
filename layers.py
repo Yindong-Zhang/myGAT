112d7fbc422aba07170f252696e52d7247bfe3d0
yindong <so_go_flying@163.com>
2019-03-25 17:50:04 +0800

    1. reduce memory usage.

--- layers.py
+++ layers.py
@@ -9,40 +9,37 @@ class GraphAttentionLayer(nn.Module):
     Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
 
-    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+    def __init__(self, in_features, out_features, dropout, alpha):
         super(GraphAttentionLayer, self).__init__()
         self.dropout = dropout
         self.in_features = in_features
         self.out_features = out_features
         self.alpha = alpha
-        self.concat = concat
 
-        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
+        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features), dtype= torch.float))
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+        self.a_1 = nn.Parameter(torch.zeros(size=(out_features, 1), dtype= torch.float))
+        nn.init.xavier_uniform_(self.a_1.data, gain=1.414)
+        self.a_2 = nn.Parameter(torch.zeros(size=(out_features, 1), dtype= torch.float))
+        nn.init.xavier_uniform_(self.a_2.data, gain=1.414)
 
         self.leakyrelu = nn.LeakyReLU(self.alpha)
 
     def forward(self, input, adj):
-        h = torch.mm(input, self.W)
-        N = h.size()[0]
+        h = torch.matmul(input, self.W)
 
-        a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
-        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        logit_1= torch.matmul(h, self.a_1)
+        logit_2= torch.matmul(h, self.a_2)
+        logits= logit_1 + logit_2.permute(1, 0)
+        e= self.leakyrelu(logits)
+        zero_vec = -9e15* e.new_tensor([1., ])
+        e = torch.where(adj > 0, e, zero_vec)
 
-        zero_vec = -9e15*torch.ones_like(e)
-        attention = torch.where(adj > 0, e, zero_vec)
-        attention = F.softmax(attention, dim=1)
+        attention = F.softmax(e, dim= -1)
         attention = F.dropout(attention, self.dropout, training=self.training)
+        h_out = torch.mm(attention, h)
 
-
-        h_prime = torch.matmul(attention, h)
-
-        if self.concat:
-            return F.elu(h_prime)
-        else:
-            return h_prime
+        return F.elu(h_out)
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
