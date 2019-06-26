b9fe5f583787b103ab31700697d02afc3ba06088
yindong <so_go_flying@163.com>
2019-06-26 19:49:39 +0800

    1. add mlp update procedure.

---
+++ eval_ppi.sh
@@ -0,0 +1,3 @@
+python eval_ppi.py --hidden 128 --nb_heads_1 4 --nb_heads_2 4 --lr 0.001 --weight_decay 1E-4 --patience 500 --print_every 1 --dropout 0.4 --batch_size 1 --order2_attention --gpu_ids 0
+
+
---
+++ min_batch_train.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python min_batch_train.py --gpu_ids 0 --lr 0.1 --weight_decay 0.0 --dropout 0.2 --num_basis 16
